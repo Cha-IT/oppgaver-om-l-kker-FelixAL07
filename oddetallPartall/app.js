bf8c951d266fada704771f97c52ef0918eb4c386
FelixAL07 <lerdahl07@gmail.com>
2024-09-06 14:44:09 +0200

    new feat

--- oddetallPartall/app.js
+++ oddetallPartall/app.js
@@ -1,4 +1,27 @@
 let numberPrint = document.getElementById("numberPrint")
 let partallBtn = document.getElementById("partallKnapp")
 let oddetallBtn = document.getElementById("oddetallKnapp")
+let tallGrid = [];
 
+for (let i = 1; i <= 100; i++) {
+    tallGrid.push(i);
+}
+
+const oddetall = tallGrid.filter(number => {
+    return number % 2 !== 0;
+});
+
+const partall = tallGrid.filter(number => {
+    return number % 2 === 0;
+});
+
+function printOddetall() {
+    numberPrint.innerHTML = oddetall
+}
+
+function printPartall() {
+    numberPrint.innerHTML = partall
+}
+
+partallBtn.onclick = printPartall
+oddetallBtn.onclick = printOddetall
